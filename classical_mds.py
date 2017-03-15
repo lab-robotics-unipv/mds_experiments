304580197afdfae3c8a175b44ff765d390641836
Moses Koledoye <moseskoledoye@gmail.com>
2017-03-15 16:53:44 +0800

    update comments

--- classical_mds.py
+++ classical_mds.py
@@ -20,7 +20,7 @@ M = 2
 #       .
 
 
-# Set up the squared proximity matrix
+# Set up the proximity and squared proximity matrices
 prox_arr = np.array([[0, 5, 3], [5, 0, 4], [3, 4, 0]])
 sqrd_prox_arr = prox_arr**2
 
@@ -29,18 +29,18 @@ sz = prox_arr.shape[0]
 cent_arr = np.eye(sz) - np.ones(sz)/sz
 B = -cent_arr.dot(sqrd_prox_arr).dot(cent_arr)/2
 
-# Determine the m largest eigenvalues and corresponding eigenvectors
+# Determine the M largest eigenvalues and corresponding eigenvectors
 eig_vals, eig_vecs = np.linalg.eig(B)
 eig_vals_vecs = zip(*sorted(zip(eig_vals, eig_vecs.T), key=operator.itemgetter(0), reverse=True)[:M])
 eig_vals, eig_vecs = map(np.array, eig_vals_vecs)
 
-# configuration X of n points/coordinates that optimise the cost function
+# Configuration X of n points/coordinates that optimise the cost function
 coords = eig_vecs.T.dot((np.eye(M)*eig_vals)**0.5)
 print(coords)
 
-# compute euclidean distances
+# Compute euclidean distances
 similairities = euclidean_distances(coords)
 print(similairities)
 
-# compare the new distance matrix with the original
+# Compare the new proximity matrix with the original
 np.testing.assert_allclose(similairities, prox_arr)
