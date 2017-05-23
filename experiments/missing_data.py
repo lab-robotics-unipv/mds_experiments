9ced37f2d495ecb13d53a863b2b7a737ea545da0
Moses Koledoye <moseskoledoye@gmail.com>
2017-05-23 17:09:23 +0200

    make plot function dynamic and move add_noise to partial call

--- experiments/missing_data.py
+++ experiments/missing_data.py
@@ -16,7 +16,7 @@ def runexperiment(func):
 		plot_hanldes = []
 		plot_labels = []
 		config = Config(no_of_anchors=4, no_of_tags=30, noise=2)
-		generate_data = partial(generate_static_nodes, algorithm='_smacof_with_anchors_single')
+		generate_data = partial(generate_static_nodes, algorithm='_smacof_with_anchors_single', add_noise=True)
 		for i in range(2):
 			if i == 0:
 				# use full distance matrix
@@ -29,22 +29,21 @@ def runexperiment(func):
 			x_axis, errors = func(**kwargs)
 			errors = np.array(errors)
 			first_q, median, third_q = evaluation.first_third_quartile_and_median(errors)
-			handles = evaluation.plot_rmse_vs_sigma(first_q, median, third_q, x_axis=x_axis, algorithm=ALGORITHMS[i])
+			handles = getattr(evaluation, 'plot_{}'.format(func.__name__))(first_q, median, third_q, x_axis=x_axis)
 			plot_hanldes.extend(handles)
 			plot_labels.extend([ALGORITHMS[i], 'IQR boundaries'])
 		plt.legend(loc='upper left', fontsize=12, labels=plot_labels, handles=plot_hanldes)
 		plt.show()
 	return wrapper
 
-
 @runexperiment
 def rmse_vs_noise(config=None, data_func=None, no_of_trials=10):
 	errors = []
-	sigmas = np.linspace(0, 4, 4)
+	sigmas = np.linspace(0, 4, 40)
 	for sigma in sigmas:
 		config.sigma = sigma
 		# generate rmse from coordinates, remembering to not pass last_n_coords to function
-		error = [evaluation.rmse(*data_func(config=config, add_noise=True)[:2]) for i in range(no_of_trials)]
+		error = [evaluation.rmse(*data_func(config=config)[:2]) for i in range(no_of_trials)]
 		errors.append(error)
 	return sigmas, errors
 
@@ -55,7 +54,7 @@ def rmse_vs_ntags(config=None, data_func=None, no_of_trials=10):
 	for nt in tag_count:
 		config.no_of_tags = nt
 		# generate rmse from coordinates, remembering to not pass last_n_coords to function
-		error = [evaluation.rmse(*data_func(config=config, add_noise=True)[:2]) for i in range(no_of_trials)]
+		error = [evaluation.rmse(*data_func(config=config)[:2]) for i in range(no_of_trials)]
 		errors.append(error)
 	errors = np.array(errors)
 	return tag_count, errors
