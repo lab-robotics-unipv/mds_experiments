38fb36cf4cc479a1453bd34126665677853ddc35
Moses Koledoye <moseskoledoye@gmail.com>
2017-05-23 16:41:39 +0200

    remove algorithm from arguments and rename sigma to noise

--- experiments/evaluation.py
+++ experiments/evaluation.py
@@ -15,17 +15,17 @@ def first_third_quartile_and_median(data):
 	return first_quartile, median, third_quartile
 
 
-def plot_rmse(first_quartile, median, third_quartile, x_axis=None, algorithm=''):
+def plot_rmse(first_quartile, median, third_quartile, x_axis=None):
 	plt.figure(1)
 	if x_axis is None:
 		x_axis = np.arange(median.shape[0])
 	color = next(COLORS)
-	plot1, = plt.plot(x_axis, median, 'k-', color=color, label=algorithm)
-	plot2 = plt.fill_between(x_axis, first_quartile, third_quartile, color=color, label=algorithm, alpha=0.3)
+	plot1, = plt.plot(x_axis, median, 'k-', color=color)
+	plot2 = plt.fill_between(x_axis, first_quartile, third_quartile, color=color, alpha=0.3)
 	axes = plt.gca()
 	return plot1, plot2, axes
 
-def plot_rmse_vs_sigma(*args, **kwargs):
+def plot_rmse_vs_noise(*args, **kwargs):
 	plot1, plot2, axes = plot_rmse(*args, **kwargs)
 	axes.set_xlabel('Measurement noise $\sigma$ [m]')
 	axes.set_ylabel('RMSE of computed configuration [m]')
@@ -37,7 +37,7 @@ def plot_rmse_vs_anchors(*args, **kwargs):
 	axes.set_ylabel('RMSE of computed configuration [m]')
 	return plot1, plot2
 
-def plot_rmse_vs_tags(*args, **kwargs):
+def plot_rmse_vs_ntags(*args, **kwargs):
 	plot1, plot2, axes = plot_rmse(*args, **kwargs)
 	axes.set_xlabel('No of tags')
 	axes.set_ylabel('RMSE of computed configuration [m]')
