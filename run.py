dc2caff3b1cb944d256d72761e0ed17b9e8dad5f
Moses Koledoye <moseskoledoye@gmail.com>
2017-06-05 14:11:48 +0200

    make experiments callable with args from command line

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
+import argparse
+
 from core.config import Config
 from experiments import missing_data, comparisons
 
 
 if __name__ == '__main__':
-	# config = Config(no_of_anchors=4, no_of_tags=30, noise=2, missingdata=True)
-	# comparisons.rmse_vs_noise(config=config, no_of_trials=5)
-
-	# comparisons.rmse_vs_anchors(config=config, no_of_trials=5)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--exp_no', type=int, required=True)
+	parser.add_argument('--nruns', type=int, required=True)
+	args = parser.parse_args()
+	exp_no, nruns = args.exp_no, args.nruns
 
 	config = Config(no_of_anchors=4, no_of_tags=30, noise=2)
-	#missing_data.rmse_vs_noise(config=config, no_of_trials=5)
-
-	missing_data.rmse_vs_ntags(config=config, no_of_trials=5)
+	if exp_no == 1:
+		config.missingdata = True
+		comparisons.rmse_vs_noise(config=config, no_of_trials=nruns)
+	elif exp_no == 2:
+		config.missingdata = True
+		comparisons.rmse_vs_anchors(config=config, no_of_trials=nruns)
+	elif exp_no == 3:
+		missing_data.rmse_vs_noise(config=config, no_of_trials=nruns)
+	elif exp_no == 4:
+		missing_data.rmse_vs_ntags(config=config, no_of_trials=nruns)
